c41f1ee985c1530d1d60f828a3985ee20e98dd52
himanshjangra1 <himanshujangra70152@gmail.com>
2026-05-11 10:58:33 +0530

    Created a database file and added schema

---
+++ db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema ({
+    email: {type: String, unique: true}, // Make email unique to avoid duplicate entries
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema ({
+    email: {type: String, unique: true}, // Make email unique to avoid duplicate entries
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+});
+
+// Create models for User, Admin, Course, and Purchase using the respective schemas
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+// Export the userModel, adminModel, courseModel, and purchaseModel to be used in other files
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
