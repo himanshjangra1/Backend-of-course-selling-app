4260e3759afd686b3f6c819edcb56e76a6bec77b
himanshjangra1 <himanshujangra70152@gmail.com>
2026-05-11 12:27:33 +0530

    Connected with MongoDB compass

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { userRouter } = require("./routes/user.js");
 const { adminRouter } = require("./routes/admin.js");
-const { courseRouter } = require("./routes/course.js")
+const { courseRouter } = require("./routes/course.js");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -17,4 +18,35 @@ app.get("/", (req, res) => {
     })
 })
 
-app.listen(3000);
+// creating a main function to connect with database and to check it its connected or not
+
+async function main() {
+    // error handling using try-catch
+    try {
+        const connection = await mongoose.connect("Put your connection string from MongoDB compass");
+        
+        console.log("connected to database");
+    }
+    catch (err) {
+        console.log("failed to connect to database due to - ", err);
+    }
+
+     /*
+    // Use the connect method to connect to the database and log a success message if the connection is successful
+    const connection = await mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/coursera-app");
+
+    // Check if the connection is successful or not
+    if (connection) { // If the connection is successful
+        console.log("Connected to the database");
+    } else { // If the connection is not successful
+        console.log("Failed to connect to the database");
+    }
+    */
+
+    // Start the server on port 3000
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+}
+
+main();
